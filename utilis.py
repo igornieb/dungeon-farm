cfe1b64c383a5dcfbaacceba05712126c46c6ba7
Igor <66256669+igornieb@users.noreply.github.com>
2023-07-20 13:23:03 +0200

    Update utilis.py

--- utilis.py
+++ utilis.py
@@ -1,4 +1,12 @@
 import math
+import pygame
+
+
+def collidegroup(sprite: pygame.sprite.Sprite, sprite_group: pygame.sprite.Group) -> bool:
+    for s in sprite_group:
+        if distance(sprite.rect.centerx, sprite.rect.centery, s.rect.centerx, s.rect.centery) < ((sprite.rect.height + sprite.rect.width)/2 * math.sqrt(2)):
+            return True
+    return False
 
 
 def distance(x1: int, y1: int, x2: int, y2: int) -> float:
