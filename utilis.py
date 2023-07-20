f8c81c9f864f4511d18d7faa3424a418bec528e0
igornieb <igornieb@gmail.com>
2023-07-20 14:56:57 +0200

    added optional argument (offset) to collidegroup

--- utilis.py
+++ utilis.py
@@ -2,9 +2,11 @@ import math
 import pygame
 
 
-def collidegroup(sprite: pygame.sprite.Sprite, sprite_group: pygame.sprite.Group) -> bool:
+def collidegroup(sprite: pygame.sprite.Sprite, sprite_group: pygame.sprite.Group, offset: tuple = (0, 0)) -> bool:
     for s in sprite_group:
-        if distance(sprite.rect.centerx, sprite.rect.centery, s.rect.centerx, s.rect.centery) < ((sprite.rect.height + sprite.rect.width)/2 * math.sqrt(2)):
+        if distance(sprite.rect.centerx + offset[0], sprite.rect.centery + offset[1], s.rect.centerx,
+                    s.rect.centery) < (
+                (sprite.rect.height + sprite.rect.width) / 2 * math.sqrt(2)):
             return True
     return False
 
